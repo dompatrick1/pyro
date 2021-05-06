55796ed7299ce6cfcff7e23087583e9cafecec84
Dominic Patrick <dom.patrick92@gmail.com>
2021-05-06 14:00:03 -0400

    updates to Player

--- react-app/src/components/Player/Player.js
+++ react-app/src/components/Player/Player.js
@@ -3,10 +3,20 @@ import "./player.css"
 
 function Player() {
     let songs = [
+        {name: "", url: "/Needle.mp3"},
+        {name: "Rock-n-Roll Never Forgets", url: "https://pyro-songs.s3.us-east-2.amazonaws.com/Bob-Seger-Rock-n-Roll-Never-Forgets.mp3"},
         {name: "Night Moves", url:'https://pyro-songs.s3.us-east-2.amazonaws.com/Bob-Seger-Night-Moves.mp3'},
-        {name: "Come To Poppa", url: "https://pyro-songs.s3.us-east-2.amazonaws.com/Bob-Seger-Come-To-Poppa.mp3"}]
+        {name: "The Fire Down Below", url: "https://pyro-songs.s3.us-east-2.amazonaws.com/Bob-Seger-The-Fire-Down-Below.mp3"},
+        {name: "Sunburst", url: "https://pyro-songs.s3.us-east-2.amazonaws.com/Bob-Seger-Sunburst.mp3"},
+        {name: "Sunspot Baby", url: "https://pyro-songs.s3.us-east-2.amazonaws.com/Bob-Seger-Sunspot-Baby.mp3"},
+        {name: "Main Street", url: "https://pyro-songs.s3.us-east-2.amazonaws.com/Bob-Seger-Main-Street.mp3"},
+        {name: "Come To Poppa", url: "https://pyro-songs.s3.us-east-2.amazonaws.com/Bob-Seger-Come-To-Poppa.mp3"},
+        {name: "Ship of Fools", url: "https://pyro-songs.s3.us-east-2.amazonaws.com/Bob-Seger-Ship-Of-Fools.mp3"},
+        {name: "Mary Lou", url: "https://pyro-songs.s3.us-east-2.amazonaws.com/Bob-Seger-Mary-Lou.mp3"}
+        ]
 
     const [songIndex, setSongIndex] = useState(0)
+    const [playing, setPlaying] = useState(true)
 
 
     const audio = document.getElementById("audio")
@@ -23,7 +33,14 @@ function Player() {
 
     function playSongs(e) {
         e.preventDefault()
-        audio.play()
+
+        if (playing === true) {
+            setPlaying(false)
+            audio.play()
+        } else {
+            setPlaying(true)
+            audio.pause()
+        }
     }
 
     function nextSong(e) {
@@ -45,19 +62,23 @@ function Player() {
 
     return (
         <div>
-            <button onClick={e => playSongs(e)}>Play</button>
+            {playing === true ?
+                <button onClick={e => playSongs(e)}>Play</button>
+            : <button onClick={e => playSongs(e)}>Pause</button>}
             <button onClick={e => nextSong(e)}>Next</button>
             <button onClick={e => prevSong(e)}>Previous</button>
             <h1>{songs[songIndex].name}</h1>
+                {songIndex === 0 ?
+                    <audio id="audio" src="/Needle.mp3"></audio>
+                :
                 <audio
                     id="audio"
-                    // onPlay={e => playSongs(e)}
                     controls
                     src={songs[songIndex].url}>
                     Your browser does not support the
                     <code>audio</code> element.
                 </audio>
-            {/* </button> */}
+                }
         </div>
     )
 }
