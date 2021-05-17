195219fd0a5d0d05019620786aff3bc171ec9646
Dominic Patrick <dom.patrick92@gmail.com>
2021-05-17 18:17:17 -0400

    heroku

--- react-app/src/components/Playlist/DeletePlaylist.js
+++ react-app/src/components/Playlist/DeletePlaylist.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import {useDispatch, useSelector} from "react-redux"
 import {getPlaylistsThunk, deletePlaylistThunk} from "../../store/playlist"
 import {deletePlaylistAlbumsThunk, getPlaylistAlbumsThunk} from "../../store/playlistAlbum"
