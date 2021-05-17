81a8ba20ae00ae1b004415bc59fd800c8047390d
Dominic Patrick <dom.patrick92@gmail.com>
2021-05-17 19:01:58 -0400

    heroku push

--- react-app/src/components/Playlist/Playlist.js
+++ react-app/src/components/Playlist/Playlist.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {createPlaylistThunk, getPlaylistThunk} from "../../store/playlist"
 import "./playlist.css"
